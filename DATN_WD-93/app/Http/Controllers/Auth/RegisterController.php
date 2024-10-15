046a1a7c73f1695ac3ae4bf17e4d29bc9178c6b0
hieuultra <147162531+hieuultra@users.noreply.github.com>
2024-10-15 20:20:45 +0700

    up

--- DATN_WD-93/app/Http/Controllers/Auth/RegisterController.php
+++ DATN_WD-93/app/Http/Controllers/Auth/RegisterController.php
@@ -50,8 +50,11 @@ class RegisterController extends Controller
     {
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
+            'phone' => ['required', 'string', 'max:255'],
+            'address' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
             'password' => ['required', 'string', 'min:8', 'confirmed'],
+            'image' => ['required', 'file', 'mimes:jpeg,png,jpg,gif,svg', 'max:2048'],
         ]);
     }
 
@@ -63,10 +66,21 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        // Xử lý việc upload ảnh nếu có
+        $imagePath = null;
+        if (isset($data['image']) && $data['image'] instanceof \Illuminate\Http\UploadedFile) {
+            $imageName = time() . '.' . $data['image']->extension();
+            $data['image']->move(public_path('upload'), $imageName);
+            $imagePath = $imageName;
+        }
+
         return User::create([
             'name' => $data['name'],
+            'phone' => $data['phone'],
+            'address' => $data['address'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'image' => $imagePath,
         ]);
     }
 }
